631c98bc60e1a2257acaf1b455660e51708d3636
hazelrosenberg <hazelro@umich.edu>
2022-04-15 19:08:33 -0400

    deleted out some comments to clean up code visually

--- spotify.py
+++ spotify.py
@@ -71,11 +71,6 @@ def get_song_ids():
 
 
 def main():
-    #token = spotipy.oauth2.SpotifyClientCredentials(client_id='c2b8ee04a2a045a9bb74e3c7c3451b0a', client_secret='c82da9cec8804c83b96ffb0679ad280a')
-
-    #cache_token = token.get_access_token()
-    #spotify = spotipy.Spotify(cache_token)
-
     #SETS UP THE DATABASE
     cur, conn = setUpDatabase('music.db')
 
@@ -95,30 +90,19 @@ def main():
 
 main()
 
+
+
+
+
 #function to get list of ids of songs
 
 #function to get genres of songs from ids
 
 #function to put in DB
 
-#client_credentials_manager = SpotifyClientCredentials(client_id=cid, client_secret=secret)
-#sp = spotipy.Spotify(client_credentials_manager = client_credentials_manager)
-#add token to the url, writing my version of the function here
+#token = spotipy.oauth2.SpotifyClientCredentials(client_id='c2b8ee04a2a045a9bb74e3c7c3451b0a', client_secret='c82da9cec8804c83b96ffb0679ad280a')
 
-#country_table = [['USA', '01'], ['Canada', '02']] #this is our country table we will use when we join tables later, I just wrote in random countries we may want to work with
-'''should we use top 100 as our usa list? I do not think 
-there is a billboard page specific to the usa.
-we can come up with other countries and I can just rename the 
-functions'''
-#def spotify_100usa(tag, offset, cur):
- #   token = ??
-  #  base_url = #FIND BASE URL THEN +TAG +END OF URL
-   # bounds = {'limit': 25, 'offset': offset, 'access_token': token}
-    #search = requests.get(base_url, params = bounds)
-    #json1 = search.json()
-    #for_db = []
-    #loop through and put items we need  into a list
+    #cache_token = token.get_access_token()
+    #spotify = spotipy.Spotify(cache_token)
 
-#maybe copy paste functions and do other countries?
-#next: use the select join thing for the tables
-#create visualizations
+    
