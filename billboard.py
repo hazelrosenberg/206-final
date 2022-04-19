de46babfc8598177713f9e536e455dac05c83e04
ssella1 <ssella@umich.edu>
2022-04-19 17:16:30 -0400

    created databases

--- billboard.py
+++ billboard.py
@@ -11,8 +11,8 @@ def billboard_100canada(): #are we doing usa
     resp = requests.get(url)
     if resp.ok:
         soup= BeautifulSoup(resp.content, 'html.parser')
-    titles = soup.find_all('a', class_ = 'c-title__link lrv-a-unstyle-link')
-    artists = soup.find_all('p', class_ = 'c-tagline a-font-primary-l a-font-primary-m@mobile-max lrv-u-color-black u-color-white@mobile-max lrv-u-margin-tb-00 lrv-u-padding-t-025 lrv-u-margin-r-150')
+    titles = soup.find_all('h3', class_ = 'c-title a-no-trucate-a-font-primary-bold-s u-letter-spacing-0021 lrv-u-font-size-18@tablet lrv-u-font-size-16 u-line-height-125 u-line-height-normal@mobile-max a-truncate-ellipsis u-max-width-330 u-max-width-230@tablet-only')
+    artists = soup.find_all('span', class_ = 'c-label a-no-trucate a-font-primary-s lrv-u-fofnt-size-14@mobile-max u-letter-spacing-0021 lrv-u-display-block a-truncate-ellpisis-2line u-max-width-330 u-max-width-230@tablet-only')
     for i in range(len(titles[:25])):
         title = titles[i].text.strip()
         artist = artists[i].text.strip()
@@ -38,24 +38,24 @@ def billboard_100canada(): #are we doing usa
         song_titles.append(titles)'''
     return song_titlesca
 
-def billboard_100japan():
-    song_titlesjapan = []
-    url = input('https://www.billboard.com/charts/japan-hot-100/') #whatever url we will use 
+def billboard_100usa():
+    song_titlesusa = []
+    url = input('https://www.billboard.com/charts/the-billboard-hot-100/') #whatever url we will use 
     resp = requests.get(url)
     if resp.ok:
         soup= BeautifulSoup(resp.content, 'html.parser')
-    titles = soup.find_all('a', class_ = 'c-title__link lrv-a-unstyle-link')
-    artists = soup.find_all('p', class_ = 'c-tagline a-font-primary-l a-font-primary-m@mobile-max lrv-u-color-black u-color-white@mobile-max lrv-u-margin-tb-00 lrv-u-padding-t-025 lrv-u-margin-r-150')
+    titles = soup.find_all('h3', class_ = 'c-title a-no-trucate-a-font-primary-bold-s u-letter-spacing-0021 lrv-u-font-size-18@tablet lrv-u-font-size-16 u-line-height-125 u-line-height-normal@mobile-max a-truncate-ellipsis u-max-width-330 u-max-width-230@tablet-only')
+    artists = soup.find_all('span', class_ = 'c-label a-no-trucate a-font-primary-s lrv-u-fofnt-size-14@mobile-max u-letter-spacing-0021 lrv-u-display-block a-truncate-ellpisis-2line u-max-width-330 u-max-width-230@tablet-only')
     for i in range(len(titles[:25])):
         title = titles[i].text.strip()
         artist = artists[i].text.strip()
         tuple1 = (title, artist)
-        song_titlesjapan.append(tuple1)
+        song_titlesusa.append(tuple1)
     for i in range(len(titles[25:50])):
         title = titles[i].text.strip()
         artist = artists[i].text.strip()
         tuple1 = (title, artist)
-        song_titlesjapan.append(tuple1)
+        song_titlesusa.append(tuple1)
     '''for i in range(len(titles[50:75])):
         title = titles[i].text.strip()
         artist = artists[i].text.strip()
@@ -66,8 +66,26 @@ def billboard_100japan():
         artist = artists[i].text.strip()
         tuple1 = (title, artist)
         song_titlesuk.append(tuple1)'''
-    return song_titlesjapan#this will be what has all of the scraped data in it
-    
+    return song_titlesusa#this will be what has all of the scraped data in it
+def  create_cabb_table(data, cur, conn, offset=0):
+    cur.execute('CREATE TABLE IF NOT EXISTS CanadaBBTable (id INTEGER PRIMARY KEY, bbsong_name TEXT UNIQUE, bbartist_name TEXT UNIQUE')
+    conn.commit()
+    r = offset + 25
+    for i in range(offset, r):
+        song_info = data[i]
+        cur.execute('INSERT OR IGNORE INTO CanadaBBTable (id, bbsong_name, bbartist_name) VALUES (?,?,?)', (i, song_info[0], song_info[1]))
+        conn.commit()
+    pass
+def create_usabb_table(data, cur, conn, offset=0):
+    cur.execute('CREATE TABLE IF NOT EXISTS UsaBBTable (id INTEGER PRIMARY KEY, bbsong_nameusa TEXT UNIQUE, bbartist_nameusa TEXT UNIQUE')
+    r = offset +  25
+    for i in range(offset, r):
+        song_info = data[i]
+        cur.execute('INSERT OR IGNORE INTO UsaBBTable (id, bbsong_nameusa, bbartist_nameusa) VALUES (?,?,?)' (i, song_info[0], song_info[1]))
+        conn.commit()
+    pass
+
+
 def bsDatabase():
     #CREATE DATABASE USING INSERT 
     '''conn = sqlite3.connect('your_database.sqlite3', check_same_thread=False)  #make a database in sqlite3 first?
