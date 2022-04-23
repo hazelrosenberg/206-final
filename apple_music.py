36da495fdf30dc82f3f19939e9069cf09de39e35
hazelrosenberg <hazelro@umich.edu>
2022-04-23 14:25:10 -0400

    replicated functions from spotify.py to create tables, write calculations to text files, create pie charts

--- apple_music.py
+++ apple_music.py
@@ -5,6 +5,8 @@ import pandas as pd
 import os
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
+import matplotlib.pyplot as plt
+from textwrap import wrap
 
 def setUpDatabase(db_name):
     '''Connects to the database with the provided name (db_name).'''
@@ -92,6 +94,69 @@ def createCanadaTable(data, cur, conn, offset=0):
         conn.commit()    
     pass
 
+def getCanadaGenreCounts(cur):
+    '''Uses the cursor object to select all the genres from the Genres table in the database (music.db), and then selects the count of how many songs of each genre are in the CanadaAppleMusic table by joining the Genres and CanadaAppleMusic tables on the genre ids. Returns the count and name of each genre as a tuple in a list of tuples.'''
+    l = []
+    cur.execute('SELECT genre FROM Genres')
+    x = cur.fetchall()
+    for item in x:
+        cur.execute('SELECT COUNT(genre_id) FROM CanadaAppleMusic JOIN Genres ON CanadaAppleMusic.genre_id = Genres.id WHERE Genres.genre = ?', (item[0], ))
+        l.append((cur.fetchone()[0], item[0]))
+    return l
+    pass
+
+def getUSAGenreCounts(cur):
+    '''Uses the cursor object to select all the genres from the Genres table in the database (music.db), and then selects the count of how many songs of each genre are in the USAAppleMusic table by joining the Genres and USAAppleMusic tables on the genre ids. Returns the count and name of each genre as a tuple in a list of tuples.'''
+    l = []
+    cur.execute('SELECT genre FROM Genres')
+    x = cur.fetchall()
+    for item in x:
+        cur.execute('SELECT COUNT(genre_id) FROM USAAppleMusic JOIN Genres ON USAAppleMusic.genre_id = Genres.id WHERE Genres.genre = ?', (item[0], ))
+        l.append((cur.fetchone()[0], item[0]))
+    return l
+    pass
+
+def writeCalculatedDataToFile(data, filename):
+    '''Accepts list of tuples for a playlist that include each genre and the number of songs of that genre in the playlist (data), and a file name to write the calculations to (filename). Creates the file in the directory, named after the filename parameter. Performs calculations and writes them to the file, then closes the file.'''
+    dir = os.path.dirname(__file__)
+    outFile = open(os.path.join(dir, filename), 'w')
+    total = 0
+    for item in data:
+        total += item[0]
+    outFile.write('Genre,Number of Songs,Percent of Total')
+    for item in data:
+        perc = round((item[0] / total) * 100)
+        outFile.write('\n'+item[1]+','+str(item[0])+','+str(perc)+'%')
+    outFile.write('\n')
+    outFile.write('\nTotal Number of Songs: '+str(total))
+    outFile.close()
+    pass
+
+def createPieChart(data, title):
+    '''Accepts a list of tuples for a playlist that include each genre and the number of songs of that genre in the playlist (data), and a title for the pie chart (title). Omits genres with 0 songs in the playlist from being included in the pie chart and makes a footnote of which genres had no songs. Creates a pie chart with the genres that had more than 0 songs.'''
+    data = sorted(data, reverse=True)
+    no_zeros = [i for i in data if i[0] != 0]
+    zeros = ', '.join([i[1] for i in data if i[0] == 0])
+    labels = []
+    sizes = []
+    total = 0
+    for tup in no_zeros:
+        labels.append(tup[1])
+        total += tup[0]
+    for tup in no_zeros:
+        size = (tup[0]/total) * 360
+        sizes.append(size)
+    colors = ['#e6ff00', '#1b96c6', '#ff952a', '#61ff68', '#e258c3', '#71f7ff', '#8682e6', '#ff646a', '#00b5af', '#ffe65b', '#5dc480']
+    plt.figure(figsize=(7,7))
+    plt.pie(sizes, labels=labels, colors=colors, autopct='%1.1f%%', pctdistance=0.85, textprops={'fontsize': 12})
+    plt.axis('equal')
+    plt.title('\n'.join(wrap(title,60)), fontsize=14, fontweight='bold')
+    footnote = f'Genres with no songs in the top 50 this week: {zeros}'
+    plt.annotate(footnote, xy=(-0.1,-0.1), xycoords='axes fraction', fontsize=9)
+    #plt.tight_layout()
+    plt.show()
+    pass
+
 
 def main():
     #SETS UP THE DATABASE
@@ -125,165 +190,22 @@ def main():
     except:
         createCanadaTable(canada_data, cur, conn)
 
+    #GET GENRE COUNTS FROM DATABASE
+    canada_genres = getCanadaGenreCounts(cur)
+    usa_genres = getUSAGenreCounts(cur)
+
+    #WRITE CALCULATED DATA TO TEXT FILES
+    c_title = 'appleMusicCalculationsCanada.txt'
+    writeCalculatedDataToFile(canada_genres, c_title)
+    u_title = 'appleMusicCalculationsUSA.txt'
+    writeCalculatedDataToFile(usa_genres, u_title)
+
+    #CREATE PIE CHARTS SHOWING PROPORTIONS OF EACH GENRE BY NUMBER OF SONGS
+    canada_title = 'Proportion of Genres of Top 50 Most Popular Songs in Canada on Apple Music This Week'
+    createPieChart(canada_genres, canada_title)
+    usa_title = 'Proportion of Genres of Top 50 Most Popular Songs in the USA on Apple Music This Week'
+    createPieChart(usa_genres, usa_title)
+
 
 if __name__ == '__main__':
     main()
-
-
-
-#url = 'https://kworb.net/charts/apple_s/us.html'   
-#r = requests.get(url)
-#soup = BeautifulSoup(r.content, 'html.parser')
-#tags =soup.find_all('td', class_ = 'mp text')          
-#collect_info = []
-#for tag in tags:
-    #info = tag.text
-    #info = info.split(' - ')
-    #song_name = info[1]
-    #artist = info[0]
-    #collect_info.append((song_name, artist))
-
-#EXTRACT EACH STRING AND SPLIT WITH ' - '
-#IN EACH OUTPUT, 0 INDEX IS THE ARTIST AND 1 INDEX IS THE SONG TITLE
-#ONE WILL BE CALLED ARTIST AND THE OTHER CALLED SONG
-#PRECEEDING CREATING DICTIONARY AND DATABASE
-#delimiter = ' - '
-    #new_collect = collect_info[0].split(delimiter)
-    #artist = new_collect[0]
-    #song = new_collect[1]
-    #song = collect_info[1]
-#print(artist)   
-#print(song)
-
-#after splitting up lists...
-#UNCOMMENT AND THIS WILL CREATE SPREADSHEET WITH ALL DATA
-
-#usa_dictionary = {'song_title_usa': song, 'artist_name_usa': artist}
-#data_frame = pd.DataFrame(usa_dictionary)
-#to_csv =  data_frame.to_csv('/Users/sydneysella/billboard_usa_data.csv')
-
-#UNCOMMENT AND THIS WILL SAVE ALL TO A DATABASE
-#db = mysql.connector.connect(database = 'music')
-#cursor = db.cursor()
-#insert and stuff, watch rest of video after database stuff is sorted
-
-
-
-
-
-
-
-
-
-
-#import requests
-#import sqlite3
-#import matplotlib.pyplot as plt
-#from bs4 import BeautifulSoup
-#import os
-#import mysql.connector
-
-#def setUpDatabase(db_name):
-    #'''Sets up the database with the provided name (db_name).'''
-    #path = os.path.dirname(os.path.abspath(__file__))
-    #conn  = sqlite3.connect(path+'/'+db_name)
-    #cur = conn.cursor()
-    #return cur, conn
-
-#def billboard_100canada(): #are we doing usa
-    #song_titlesca = []
-    #url = input('https://www.billboard.com/charts/canadian-hot-100/') #whatever url we will use 
-    #resp = requests.get(url)
-    #if resp.ok:
-        #soup= BeautifulSoup(resp.content, 'html.parser')
-    #titles = soup.find_all('h3', class_ = 'c-title a-no-trucate-a-font-primary-bold-s u-letter-spacing-0021 lrv-u-font-size-18@tablet lrv-u-font-size-16 u-line-height-125 u-line-height-normal@mobile-max a-truncate-ellipsis u-max-width-330 u-max-width-230@tablet-only')
-    #artists = soup.find_all('span', class_ = 'c-label a-no-trucate a-font-primary-s lrv-u-fofnt-size-14@mobile-max u-letter-spacing-0021 lrv-u-display-block a-truncate-ellpisis-2line u-max-width-330 u-max-width-230@tablet-only')
-    #for i in range(len(titles[:25])):
-        #title = titles[i].text.strip()
-        #artist = artists[i].text.strip()
-        #tuple1 = (title, artist)
-        #song_titlesca.append(tuple1)
-    #for i in range(len(titles[25:50])):
-        #title = titles[i].text.strip()
-        #artist = artists[i].text.strip()
-        #tuple1 = (title, artist)
-        #song_titlesca.append(tuple1)
-    #for i in range(len(titles[50:75])):
-        #title = titles[i].text.strip()
-        #artist = artists[i].text.strip()
-        #tuple1 = (title, artist)
-        #song_titlesca.append(tuple1)
-    #for i in range(len(titles[75:])):
-        #title = titles[i].text.strip()
-        #artist = artists[i].text.strip()
-        #tuple1 = (title, artist)
-        #song_titlesca.append(tuple1)
-    #for tag in tags[:25]:
-        #titles = tag.get()
-        #song_titles.append(titles)
-    #return song_titlesca
-
-#def billboard_100usa():
-    #song_titlesusa = []
-    #url = input('https://www.billboard.com/charts/the-billboard-hot-100/') #whatever url we will use 
-    #resp = requests.get(url)
-    #if resp.ok:
-        #soup= BeautifulSoup(resp.content, 'html.parser')
-    #titles = soup.find_all('h3', class_ = 'c-title a-no-trucate-a-font-primary-bold-s u-letter-spacing-0021 lrv-u-font-size-18@tablet lrv-u-font-size-16 u-line-height-125 u-line-height-normal@mobile-max a-truncate-ellipsis u-max-width-330 u-max-width-230@tablet-only')
-    #artists = soup.find_all('span', class_ = 'c-label a-no-trucate a-font-primary-s lrv-u-fofnt-size-14@mobile-max u-letter-spacing-0021 lrv-u-display-block a-truncate-ellpisis-2line u-max-width-330 u-max-width-230@tablet-only')
-    #for i in range(len(titles[:25])):
-        #title = titles[i].text.strip()
-        #artist = artists[i].text.strip()
-        #tuple1 = (title, artist)
-        #song_titlesusa.append(tuple1)
-    #for i in range(len(titles[25:50])):
-        #title = titles[i].text.strip()
-        #artist = artists[i].text.strip()
-        #tuple1 = (title, artist)
-        #song_titlesusa.append(tuple1)
-    #for i in range(len(titles[50:75])):
-        #title = titles[i].text.strip()
-        #artist = artists[i].text.strip()
-        #tuple1 = (title, artist)
-        #song_titlesuk.append(tuple1)
-    #for i in range(len(titles[75:])):
-        #title = titles[i].text.strip()
-        #artist = artists[i].text.strip()
-        #tuple1 = (title, artist)
-        #song_titlesuk.append(tuple1)
-    #return song_titlesusa#this will be what has all of the scraped data in it
-#def  create_cabb_table(data, cur, conn, offset=0):
-    #cur.execute('CREATE TABLE IF NOT EXISTS CanadaBBTable (id INTEGER PRIMARY KEY, bbsong_name TEXT UNIQUE, bbartist_name TEXT UNIQUE')
-    #conn.commit()
-    #r = offset + 25
-    #for i in range(offset, r):
-        #song_info = data[i]
-        #cur.execute('INSERT OR IGNORE INTO CanadaBBTable (id, bbsong_name, bbartist_name) VALUES (?,?,?)', (i, song_info[0], song_info[1]))
-        #conn.commit()
-    #pass
-#def create_usabb_table(data, cur, conn, offset=0):
-    #cur.execute('CREATE TABLE IF NOT EXISTS UsaBBTable (id INTEGER PRIMARY KEY, bbsong_nameusa TEXT UNIQUE, bbartist_nameusa TEXT UNIQUE')
-    #r = offset +  25
-    #for i in range(offset, r):
-        #song_info = data[i]
-        #cur.execute('INSERT OR IGNORE INTO UsaBBTable (id, bbsong_nameusa, bbartist_nameusa) VALUES (?,?,?)', (i, song_info[0], song_info[1]))
-        #conn.commit()
-    #pass
-#def add_genres(db_filename):
-    #path = os.path.dirname(os.path.abspath(__file__))
-    #conn = sqlite3.connect(path+'/'+db_filename)
-    #cur = conn.cursor()
-    #cur.execute('SELECT UsaBBTable.id from UsaBBTable JOIN Genres ')
-    #somehow add genres to the table but make the songs the same?
-   
-
-
-
-#def main():
-    #sets up database
-    #cur, conn = setUpDatabase('music.db')
-    
-   
-
-#if __name__ == '__main__':
-    #main()
