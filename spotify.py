103b532074fcd972b614ff8b185113a94e564aa4
hazelrosenberg <hazelro@umich.edu>
2022-04-23 11:37:32 -0400

    spotify calculations written out to text file

--- spotify.py
+++ spotify.py
@@ -121,9 +121,15 @@ def writeCalculatedDataToFile(data, filename):
     ''''''
     dir = os.path.dirname(__file__)
     outFile = open(os.path.join(dir, filename), 'w')
-    outFile.write('Genre,Number of Songs')
+    total = 0
     for item in data:
-        outFile.write('\n'+item[1]+','+str(item[0]))
+        total += item[0]
+    outFile.write('Genre,Number of Songs,Percent of Total')
+    for item in data:
+        perc = round((item[0] / total) * 100)
+        outFile.write('\n'+item[1]+','+str(item[0])+','+str(perc)+'%')
+    outFile.write('\n')
+    outFile.write('\nTotal Number of Songs: '+str(total))
     outFile.close()
     pass
 
@@ -197,9 +203,9 @@ def main():
 
     #CREATE PIE CHARTS SHOWING PROPORTIONS OF EACH GENRE BY NUMBER OF SONGS
     canada_title = 'Proportion of Genres of Top 50 Most Popular Songs in Canada on Spotify This Week'
-    #createPieChart(canada_genres, canada_title)
+    createPieChart(canada_genres, canada_title)
     usa_title = 'Proportion of Genres of Top 50 Most Popular Songs in the USA on Spotify This Week'
-    #createPieChart(usa_genres, usa_title)
+    createPieChart(usa_genres, usa_title)
     pass
 
 
@@ -210,4 +216,4 @@ if __name__ == '__main__':
 #token = spotipy.oauth2.SpotifyClientCredentials(client_id='c2b8ee04a2a045a9bb74e3c7c3451b0a', client_secret='c82da9cec8804c83b96ffb0679ad280a')
 
 #cache_token = token.get_access_token()
-#spotify = spotipy.Spotify(cache_token) 
+#sp = spotipy.Spotify(cache_token) 
