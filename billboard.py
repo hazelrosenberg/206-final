dffa3bc089b617cc8725d7cc8d915d0f6dac5f80
ssella1 <ssella@umich.edu>
2022-04-23 12:14:29 -0400

    fixed script

--- billboard.py
+++ billboard.py
@@ -1,9 +1,64 @@
 import requests
 import sqlite3
+from bs4 import BeautifulSoup
+import pandas as pd
+import mysql.connector
+#from cottage inn example in runestone for apple music usa charts
+url = 'https://kworb.net/charts/apple_s/us.html'   
+r = requests.get(url)
+soup = BeautifulSoup(r.content, 'html.parser')
+tags =soup.find_all('td', class_ = 'mp text')          
+collect_info = []
+for  tag in tags:
+    info = tag.text
+    collect_info.append(info)
+    new_collect = []
+    for item in collect_info:
+        new_collect.extend(item.split(' - '))
+        artist = new_collect[::2]
+        song = new_collect[1::2]
+
+
+
+#EXTRACT EACH STRING AND SPLIT WITH ' - '
+#IN EACH OUTPUT, 0 INDEX IS THE ARTIST AND 1 INDEX IS THE SONG TITLE
+#ONE WILL BE CALLED ARTIST AND THE OTHER CALLED SONG
+#PRECEEDING CREATING DICTIONARY AND DATABASE
+'''delimiter = ' - '
+    new_collect = collect_info[0].split(delimiter)
+    artist = new_collect[0]
+    song = new_collect[1]
+    #song = collect_info[1]
+print(artist)   
+print(song)'''
+
+#after splitting up lists...
+#UNCOMMENT AND THIS WILL CREATE SPREADSHEET WITH ALL DATA
+
+usa_dictionary = {'song_title_usa': song, 'artist_name_usa': artist}
+data_frame = pd.DataFrame(usa_dictionary)
+to_csv =  data_frame.to_csv('/Users/sydneysella/billboard_usa_data.csv')
+
+#UNCOMMENT AND THIS WILL SAVE ALL TO A DATABASE
+'''db = mysql.connector.connect(user = 'hazels user name for the database', database = 'music')
+cursor = db.cursor()'''
+#insert and stuff, watch rest of video after database stuff is sorted
+
+
+
+
+
+
+
+
+
+
+'''import requests
+import sqlite3
 import matplotlib.pyplot as plt
 from bs4 import BeautifulSoup
 import os
-
+import mysql.connector
 
 def setUpDatabase(db_name):
     '''Sets up the database with the provided name (db_name).'''
@@ -91,13 +146,23 @@ def create_usabb_table(data, cur, conn, offset=0):
         cur.execute('INSERT OR IGNORE INTO UsaBBTable (id, bbsong_nameusa, bbartist_nameusa) VALUES (?,?,?)', (i, song_info[0], song_info[1]))
         conn.commit()
     pass
+def add_genres(db_filename):
+    path = os.path.dirname(os.path.abspath(__file__))
+    conn = sqlite3.connect(path+'/'+db_filename)
+    cur = conn.cursor()
+    cur.execute('SELECT UsaBBTable.id from UsaBBTable JOIN Genres ')
+    #somehow add genres to the table but make the songs the same?
+   
 
 
-def bsDatabase():
+
+def main():
+    #sets up database
     cur, conn = setUpDatabase('bbmusic.db')
-    #CREATE DATABASE USING INSERT 
-    '''conn = sqlite3.connect('your_database.sqlite3', check_same_thread=False)  #make a database in sqlite3 first?
-    curs = conn.cursor()
-    curs.execute("INSERT INTO your_table_name billboard='{}'".format(billboard))
-    conn.commit()'''
+    
+   
+
+if __name__ == '__main__':
+    main()'''
+
    
